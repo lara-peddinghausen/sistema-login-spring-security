075385643b9e0049016921b0609bb08c491ba579
Lara Peddinghausen <larapedd@gmail.com>
2026-05-25 19:57:30 -0300

    feat(usuario): adiciona email, senha e role na entidade Usuario

--- src/main/java/com/sistemalogin/security/model/Usuario.java
+++ src/main/java/com/sistemalogin/security/model/Usuario.java
@@ -1,19 +1,30 @@
 package com.sistemalogin.security.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
 @Data // Ajuda a reduzir código repetitivo. Equivale a usar @Getter, @Setter, @ToString, @EqualsAndHashCode e @RequiredArgsConstructor juntos.
+@Entity
+@NoArgsConstructor
+@Table(name = "usuarios") // Especifica o nome da tabela no banco de dados
 public class Usuario {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-        
-    private String username;
-    private String password;
+    
+    @Column(nullable = false, unique = true) // Garante que o email seja único e não nulo
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false)
+    private String role;
 }
