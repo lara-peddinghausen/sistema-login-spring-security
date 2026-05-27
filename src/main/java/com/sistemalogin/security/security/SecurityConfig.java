020e4b3f6b31eb3a7ec0832c106581c83d877bae
Lara Peddinghausen <larapedd@gmail.com>
2026-05-27 20:19:15 -0300

    chore(security): adiciona estrutura inicial do Spring Security

---
+++ src/main/java/com/sistemalogin/security/security/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.sistemalogin.security.security;
+
+import org.springframework.context.annotation.Configuration;
+
+// Classe de configuração. Nela vou informar quais são as regras de segurança da minha aplicação. Ex: quais páginas podem ser acessadas por qualquer usuário, quais páginas só podem ser acessadas por usuários autenticados, quais páginas só podem ser acessadas por usuários com um determinado perfil, etc.
+
+@Configuration
+public class SecurityConfig {
+    
+}
